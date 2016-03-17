e956277fe32e68e565ca4ef85faa37e199ef03fd
Ricardo Mesquita <ricardomesquita@outlook.pt>
2016-03-17 10:53:51 +0000

    Merge origin/Ivo into Ivo
    Conflicts:
    	WhereAtUM/index.php

--- WhereAtUM/index.php
+++ WhereAtUM/index.php
@@ -1,28 +1,5 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        
-<<<<<<< HEAD
-        //Eu mesquita aqui
-        
-        //ivo alteração branch AQUI
-=======
-        // Teste mesquita togfdgdfgdfgp jtyjytuyutyuty
-        
-        
-        //Segundo teste.
->>>>>>> origin/Ivo
-        ?>
-    </body>
-</html>
+<?php
+
+
+// TESTE
+?>
